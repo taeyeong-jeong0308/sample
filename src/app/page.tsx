525d8277e419bfa72975145b5907be8082eba359
taeyeong-jeong0308 <jeongtaeteong0308@gmail.com>
2025-07-08 20:23:15 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ fontSize: "2rem", textAlign: "center", marginTop: "20vh" }}>
+      안녕 인공지능 !!!
+    </div>
+  );
+}
